197bf11f5441460824efc6439e23b625b6a70948
Andrey Radchenko <andry.radchenko@gmail.com>
2019-02-18 13:08:16 +0200

    fix(BMDEV-2500): rename import
    BMDEV-2500-Provide-historical-data-for-ATR-indicator

--- src/main/java/com/bookmap/api/simple/demo/indicators/AtrTrailingStop.java
+++ src/main/java/com/bookmap/api/simple/demo/indicators/AtrTrailingStop.java
@@ -20,12 +20,12 @@ import velox.api.layer1.simplified.HistoricalModeListener;
 import velox.api.layer1.simplified.InitialState;
 import velox.api.layer1.simplified.IntervalListener;
 import velox.api.layer1.simplified.Intervals;
-import velox.api.layer1.simplified.ReceiveHistoryOption;
+import velox.api.layer1.simplified.ReceiveHistory;
 import velox.api.layer1.simplified.SnapshotEndListener;
 import velox.api.layer1.simplified.TimeListener;
 import velox.api.layer1.simplified.TradeDataListener;
 
-@ReceiveHistoryOption(shouldReceiveHistory = true)
+@ReceiveHistory(shouldReceiveHistory = true)
 @Layer1SimpleAttachable
 @Layer1StrategyName("ATR Trailing Stop")
 @Layer1ApiVersion(Layer1ApiVersionValue.VERSION1)
